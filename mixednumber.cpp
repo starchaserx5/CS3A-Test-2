101ad16be7a80555a9b1673084c98edbb49437bd
starchaserx5 <33065746+starchaserx5@users.noreply.github.com>
2018-12-03 12:37:57 -0800

    Update mixednumber.cpp

--- mixednumber.cpp
+++ mixednumber.cpp
@@ -31,3 +31,84 @@ mixedNumber& mixedNumber::operator=(const mixedNumber &other)
        copy(other);
     return *this;
 }
+
+mixedNumber& mixedNumber::operator=(const Fraction &other)
+{
+    num = other.getNum();
+    denom = other.getDenom();
+    return *this;
+}
+
+
+mixedNumber& mixedNumber::operator=(const int &other)
+{
+    Fraction temp(other);
+    *this = temp;
+    return *this;
+}
+
+mixedNumber& mixedNumber::operator=(const double &other)
+{
+    Fraction temp(other);
+    *this = temp;
+    return *this;
+}
+
+void mixedNumber::getValues(int &w, int &n, int &d)
+{
+    w = num/denom;
+    n = num%denom;
+    d = denom;
+}
+
+void mixedNumber::setValues(int w, int n, int d)
+{
+    Fraction::setValue(w*d + n, d);
+}
+
+void mixedNumber::copy(const mixedNumber &other)
+{
+    num = other.num;
+    denom = other.denom;
+}
+
+void mixedNumber::nukeEveryone()
+{
+    num = 0;
+    denom = 1;
+}
+
+ std::ostream& operator<<( std::ostream &out, const mixedNumber &m)
+{
+     int whole = m.num/m.denom, numerator = m.num % m.denom;
+     if(numerator == 0)
+         out<<whole;
+     else
+         if(whole == 0)
+             out<<numerator<<"/"<<m.denom;
+        else
+             out<<whole<<" "<<abs(numerator)<<"/"<<m.denom;
+     return out;
+}
+
+std::istream& operator>>( std::istream &in, mixedNumber &m)
+{
+     Fraction whole(0), part(0);
+
+     in>>whole;
+     if(in.peek() == ' ')
+     {
+         in>>part;
+         Fraction total(abs(whole.getNum())*part.getDenom()+part.getNum(),part.getDenom());
+         std::cout << "The total1 here is: " << total << std::endl;
+         total = total * abs(whole.getNum())/whole.getNum();
+                  std::cout << "The total2 here is: " << total << std::endl;
+         m.setValues(0,total.getNum(),total.getDenom());
+                  std::cout << "The total3 here is: " << total << std::endl;
+     }
+     else
+     m.setValues(0,whole.getNum(),whole.getDenom());
+
+
+     return in;
+}
